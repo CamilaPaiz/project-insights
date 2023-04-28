a5c5434d49694810dc90db62aa0cf07d817301af
CamilaPaiz <camilapaiz@hotmail.com>
2023-04-28 18:20:03 -0300

    feat:implementa test_sorting.py

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,14 +2,48 @@ from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    job_list_not_sorted = [
-        {"date:2000-5-5", "max_salary:2000", "min_salary:1000"},
-        {"date:2000-5-5", "max_salary:2500", "min_salary:1000"},
-        {"date:2000-5-5", "max_salary:1000", "min_salary:5000"},
+    job_list = [
+        {
+            "date_posted": "2000-5-7",
+            "max_salary": 2000,
+            "min_salary": 1000,
+        },
+        {
+            "date_posted": "2000-5-6",
+            "max_salary": 2500,
+            "min_salary": 1010,
+        },
+        {
+            "date_posted": "2000-5-8",
+            "max_salary": 1000,
+            "min_salary": 500,
+        },
     ]
-    job_list_sorted = [
-        {"date:2000-5-5", "max_salary:1000", "min_salary:1000"},
-        {"date:2000-5-5", "max_salary:2000", "min_salary:1000"},
-        {"date:2000-5-5", "max_salary:2500", "min_salary:5000"},
+    max_salary_sorted = [
+        {
+            "date_posted": "2000-5-6",
+            "max_salary": 2500,
+            "min_salary": 1010,
+        },
+        {
+            "date_posted": "2000-5-7",
+            "max_salary": 2000,
+            "min_salary": 1000,
+        },
+        {
+            "date_posted": "2000-5-8",
+            "max_salary": 1000,
+            "min_salary": 500,
+        },
     ]
-    assert sort_by(job_list_not_sorted, "max_salary") == job_list_sorted
+    min_salary_sorted = [job_list[2], job_list[0], job_list[1]]
+    date_posted_sorted = [job_list[2], job_list[0], job_list[1]]
+
+    sort_by(job_list, "max_salary")  # decrescente
+    assert job_list == max_salary_sorted
+
+    sort_by(job_list, "min_salary")  # crescente
+    assert job_list == min_salary_sorted
+
+    sort_by(job_list, "date_posted")  # decrescente
+    assert job_list == date_posted_sorted
