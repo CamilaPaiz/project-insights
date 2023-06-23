48d16ebd69bd1f85c30411a94ed02d93535aef33
CamilaPaiz <camilapaiz@hotmail.com>
2023-06-23 09:02:25 -0300

    fix:test brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -8,7 +8,3 @@ def test_brazilian_jobs():
         assert "title" in itens
         assert "salary" in itens
         assert "type" in itens
-
-
-# como mockar o retorno esperado sendo que em mocks é feita uma função que lê
-# de jobs de insight,onde ver mais exemplos de testes?
